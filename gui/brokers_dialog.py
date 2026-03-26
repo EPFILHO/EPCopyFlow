54a196b0933a4b23d88b8116bca4d07095abac14
EPFilho <elsonpalenskefilho@gmail.com>
2026-03-26 14:21:41 -0300

    Update brokers_dialog.py - 010 - "EYE"

--- gui/brokers_dialog.py
+++ gui/brokers_dialog.py
@@ -9,9 +9,22 @@ from PySide6.QtGui import QIcon, QPixmap, QStandardItemModel, QStandardItem, QCo
 
 logger = logging.getLogger(__name__)
 
-EYE_OPEN_SVG  = ''
-EYE_CLOSED_SVG = ''
+EYE_OPEN_SVG = (
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" '
+    'fill="none" stroke="#888888" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">'
+    '<ellipse cx="12" cy="12" rx="10" ry="6"/>'
+    '<circle cx="12" cy="12" r="3"/>'
+    '</svg>'
+)
 
+EYE_CLOSED_SVG = (
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" '
+    'fill="none" stroke="#888888" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">'
+    '<path d="M17.94 17.94A10.94 10.94 0 0 1 12 20C6 20 2 12 2 12a18.09 18.09 0 0 1 5.06-5.94"/>'
+    '<path d="M9.9 4.24A9.12 9.12 0 0 1 12 4c6 0 10 8 10 8a18.5 18.5 0 0 1-2.16 3.19"/>'
+    '<line x1="1" y1="1" x2="23" y2="23"/>'
+    '</svg>'
+)
 
 def svg_icon(svg_data):
     pixmap = QPixmap()
